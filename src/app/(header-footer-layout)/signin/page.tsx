6262ddc6ef6945d1f2ff22c88ea355052ffc62c9
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-11-20 17:52:14 +0300

    updating inputs

--- src/app/(header-footer-layout)/signin/page.tsx
+++ src/app/(header-footer-layout)/signin/page.tsx
@@ -36,34 +36,53 @@ const Index = () => {
           <Image src={imgLogin} alt="" />
         </div>
         <div className="lg:w-1/2 lg:pl-20 lg:pr-20  pt-5 pb-5 pl-5 pr-5 flex items-center flex-col justify-center">
-          <h1 className="text-center text-3xl font-semibold mb-8 dark:text-white text-abrandc-dark-blackish">Sign in</h1>
-          <form>
-            <label htmlFor="email" className="dark:text-white text-abrandc-dark-blackish">Email</label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              name="email"
-              required
-              className="w-full h-10 mb-2 px-5 rounded-md drop-shadow"
-              value={email}
-              autoComplete="off"
-            />
-            <label htmlFor="password" className="dark:text-white text-abrandc-dark-blackish">Password</label>
-            <input
-              type="text"
-              placeholder="Enter your password"
-              name="password"
-              required
-              className="w-full h-10 mb-2  px-5 rounded-md drop-shadow"
-              value={password}
-              autoComplete="off"
-            />
-            <Link
-              href="/signup"
-              className=" text-sm text-CustomColor1 rounded-md underline-offset-8 underline dark:text-blue-500 text-abrandc-dark-blackish"
-            >
-              Don&apos;t have an account?
-            </Link>
+          <h1 className="text-center text-3xl font-semibold mb-8 dark:text-white text-abrandc-dark-blackish">
+            Sign in
+          </h1>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <label
+                htmlFor="email"
+                className="dark:text-white text-abrandc-dark-blackish"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                required
+                className="w-full h-10 mb-2 px-5 rounded-md drop-shadow bg-abrandc-light-grey dark:bg-gray-700"
+                value={email}
+                autoComplete="off"
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="password"
+                className="dark:text-white text-abrandc-dark-blackish"
+              >
+                Password
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your password"
+                name="password"
+                required
+                className="w-full h-10 mb-2  px-5 rounded-md drop-shadow bg-abrandc-light-grey dark:bg-gray-700"
+                value={password}
+                autoComplete="off"
+              />
+            </div>
+            <div className="">
+              {" "}
+              <Link
+                href="/signup"
+                className=" text-sm text-CustomColor1 rounded-md underline-offset-8 underline dark:text-blue-500 text-abrandc-dark-blackish"
+              >
+                Don&apos;t have an account?
+              </Link>
+            </div>
 
             <div className="text-center">
               <button
