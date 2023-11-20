6a17f4bc44c2cbb85dec3d827634c6ba8b1941cb
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-11-20 17:42:08 +0300

    editing smtng

--- src/app/(header-footer-layout)/signin/page.tsx
+++ src/app/(header-footer-layout)/signin/page.tsx
@@ -30,16 +30,15 @@ const Index = () => {
   //     // }
   //   };
   return (
-    <div className="h-full p-8 flex items-center justify-center" >
-
-      <div className="flex gap-1 w-5/6 h-5/6 drop-shadow-lg  " style={{ backgroundColor: '#F5F5F5', }}>
+    <div className="h-full p-8 flex items-center justify-center ">
+      <div className="flex gap-1 w-5/6 h-5/6 drop-shadow-lg rounded bg-abrandc-light-grey dark:bg-abrandc-dark-grey">
         <div className="hidden lg:inline-flex w-1/2">
           <Image src={imgLogin} alt="" />
         </div>
         <div className="lg:w-1/2 lg:pl-20 lg:pr-20  pt-5 pb-5 pl-5 pr-5 flex items-center flex-col justify-center">
-          <h1 className="text-center text-3xl font-semibold mb-8">Sign in</h1>
+          <h1 className="text-center text-3xl font-semibold mb-8 dark:text-white text-abrandc-dark-blackish">Sign in</h1>
           <form>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" className="dark:text-white text-abrandc-dark-blackish">Email</label>
             <input
               type="email"
               placeholder="Enter your email"
@@ -48,9 +47,8 @@ const Index = () => {
               className="w-full h-10 mb-2 px-5 rounded-md drop-shadow"
               value={email}
               autoComplete="off"
-
             />
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password" className="dark:text-white text-abrandc-dark-blackish">Password</label>
             <input
               type="text"
               placeholder="Enter your password"
@@ -62,7 +60,7 @@ const Index = () => {
             />
             <Link
               href="/signup"
-              className=" text-sm text-CustomColor1 rounded-md underline-offset-8 underline"
+              className=" text-sm text-CustomColor1 rounded-md underline-offset-8 underline dark:text-blue-500 text-abrandc-dark-blackish"
             >
               Don&apos;t have an account?
             </Link>
@@ -80,7 +78,6 @@ const Index = () => {
         </div>
       </div>
     </div>
-
   );
 };
 
